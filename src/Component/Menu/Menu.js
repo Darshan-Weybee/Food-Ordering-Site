a2e12f4d8074e9779d8d9661a9c152d79dce8eb9
Darshan Kagathara <darshan@webtheuniverse.com>
2022-09-29 11:31:17 +0530

    Updated

--- src/Component/Menu/Menu.js
+++ src/Component/Menu/Menu.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
+// import { Carousel } from 'react-responsive-carousel';
 
 const menu = [
     {
